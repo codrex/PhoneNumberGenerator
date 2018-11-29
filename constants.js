3811301909fdb6b66ecf0b65801a60d97819742a
Rex Ogbemudia <rex.ogbemudia@andela.com>
2018-11-29 13:19:57 +0100

    move error messages to constants

--- constants.js
+++ constants.js
@@ -5,6 +5,8 @@ const SORT_ORDER_DESC = 'DESC';
 const NUMBERS_MUST_BE_AN_ARRAY = 'numbers must be an array';
 const GET_NUMBER_ERROR_MSG = 'An error occurred while fetching phone numbers';
 const GEN_NUMBERS_ERROR_MSG = 'An error occurred while generating phone numbers';
+const NOT_FOUND_MSG = '404: Page not found';
+const SERVER_ERR_MSG = 'Server error';
 
 module.exports = {
   NUMBER_LEN,
@@ -14,4 +16,6 @@ module.exports = {
   GEN_NUMBERS_ERROR_MSG,
   SORT_ORDER_ASC,
   SORT_ORDER_DESC,
+  NOT_FOUND_MSG,
+  SERVER_ERR_MSG,
 };
